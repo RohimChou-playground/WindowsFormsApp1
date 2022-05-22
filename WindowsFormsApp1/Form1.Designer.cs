2bb2b077e7d038dc0b143d8185c618de593551de
rohim <rohim@gmail.com>
2022-05-22 22:47:09 +0800

    Initialize Project

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace WindowsFormsApp1 {
+  partial class Form1 {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      this.button1 = new System.Windows.Forms.Button();
+      this.listView1 = new System.Windows.Forms.ListView();
+      this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+      this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+      this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+      this.button2 = new System.Windows.Forms.Button();
+      this.treeView1 = new System.Windows.Forms.TreeView();
+      this.statusStrip1.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+      this.splitContainer1.Panel1.SuspendLayout();
+      this.splitContainer1.Panel2.SuspendLayout();
+      this.splitContainer1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(438, 36);
+      this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(158, 56);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "button1";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // listView1
+      // 
+      this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.listView1.HideSelection = false;
+      this.listView1.Location = new System.Drawing.Point(0, 0);
+      this.listView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.listView1.Name = "listView1";
+      this.listView1.Size = new System.Drawing.Size(838, 569);
+      this.listView1.TabIndex = 1;
+      this.listView1.UseCompatibleStateImageBehavior = false;
+      this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+      this.listView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listView1_KeyDown);
+      // 
+      // statusStrip1
+      // 
+      this.statusStrip1.ImageScalingSize = new System.Drawing.Size(36, 36);
+      this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+      this.statusStrip1.Location = new System.Drawing.Point(0, 702);
+      this.statusStrip1.Name = "statusStrip1";
+      this.statusStrip1.Padding = new System.Windows.Forms.Padding(2, 0, 14, 0);
+      this.statusStrip1.Size = new System.Drawing.Size(1288, 48);
+      this.statusStrip1.TabIndex = 2;
+      this.statusStrip1.Text = "statusStrip1";
+      // 
+      // toolStripStatusLabel1
+      // 
+      this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+      this.toolStripStatusLabel1.Size = new System.Drawing.Size(269, 37);
+      this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+      // 
+      // splitContainer1
+      // 
+      this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.splitContainer1.BackColor = System.Drawing.SystemColors.ControlLight;
+      this.splitContainer1.Location = new System.Drawing.Point(12, 130);
+      this.splitContainer1.Name = "splitContainer1";
+      // 
+      // splitContainer1.Panel1
+      // 
+      this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+      // 
+      // splitContainer1.Panel2
+      // 
+      this.splitContainer1.Panel2.Controls.Add(this.listView1);
+      this.splitContainer1.Size = new System.Drawing.Size(1264, 569);
+      this.splitContainer1.SplitterDistance = 420;
+      this.splitContainer1.SplitterWidth = 6;
+      this.splitContainer1.TabIndex = 3;
+      // 
+      // button2
+      // 
+      this.button2.Location = new System.Drawing.Point(12, 36);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(158, 56);
+      this.button2.TabIndex = 4;
+      this.button2.Text = "button2";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // treeView1
+      // 
+      this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.treeView1.Location = new System.Drawing.Point(0, 0);
+      this.treeView1.Name = "treeView1";
+      this.treeView1.Size = new System.Drawing.Size(420, 569);
+      this.treeView1.TabIndex = 0;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1288, 750);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.statusStrip1);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.splitContainer1);
+      this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.statusStrip1.ResumeLayout(false);
+      this.statusStrip1.PerformLayout();
+      this.splitContainer1.Panel1.ResumeLayout(false);
+      this.splitContainer1.Panel2.ResumeLayout(false);
+      ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+      this.splitContainer1.ResumeLayout(false);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.ListView listView1;
+    private System.Windows.Forms.StatusStrip statusStrip1;
+    private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    private System.Windows.Forms.SplitContainer splitContainer1;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.TreeView treeView1;
+  }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1 {
+
+  public partial class Form1 : Form {
+    public Form1() {
+      InitializeComponent();
+
+      // initialize statusstrip
+      this.statusStrip1.LayoutStyle = ToolStripLayoutStyle.HorizontalStackWithOverflow;
+      this.toolStripStatusLabel1.Alignment = ToolStripItemAlignment.Right;
+      this.toolStripStatusLabel1.Text = $"0 selected ";
+    }
+
+    private void button1_Click(object sender, EventArgs e) {
+
+      // reset columns/items
+      this.listView1.Columns.Clear();
+      this.listView1.Items.Clear();
+
+      // initialize listview
+      this.listView1.View = View.Details;
+      this.listView1.FullRowSelect = true;
+      this.listView1.Font = new Font("Consolas", 10f);
+      this.listView1.Columns.Add(new ColumnHeader { Text = "Name", Width = 110 });
+      this.listView1.Columns.Add(new ColumnHeader { Text = "Size", Width = 60 });
+      this.listView1.Columns.Add(new ColumnHeader { Text = "Date modified", Width = 150 });
+
+      // create image list
+      ImageList imageList = new ImageList();
+      imageList.Images.Add("file", Image.FromFile("./images/icons8-file-100.png"));
+      imageList.Images.Add("folder", Image.FromFile("./images/icons8-folder-100.png"));
+      imageList.ImageSize = new Size(18, 18);
+      this.listView1.SmallImageList = imageList;
+
+      // add items
+      ListViewItem lvItem1 = this.listView1.Items
+        .Add(key: "D:\\Dropbox", text: "Dropbox", imageKey: "folder");
+      lvItem1.SubItems.Add("100 MB");
+      lvItem1.SubItems.Add("5/17/2022 3:56 PM");
+
+      ListViewItem lvItem2 = this.listView1.Items
+        .Add(key: "D:\\GoogleDrive", text: "GoogleDrive", imageKey: "folder");
+      lvItem2.SubItems.Add("59 MB");
+      lvItem2.SubItems.Add("5/01/2019 8:28 PM");
+
+      ListViewItem lvItem3 = this.listView1.Items
+        .Add(key: "D:\\aaa.txt", text: "aaa.txt", imageKey: "file");
+      lvItem3.SubItems.Add("15 KB");
+      lvItem3.SubItems.Add("5/16/2022 12:30 PM");
+
+    }
+
+    private void listView1_KeyDown(object sender, KeyEventArgs e) {
+      if (e.Control && e.KeyCode == Keys.A) {
+        foreach (ListViewItem item in listView1.Items) {
+          item.Selected = true;
+        }
+      }
+
+      if (e.Control && e.KeyCode == Keys.C) {
+        List<ListViewItem> selectedItems = 
+          listView1.SelectedItems.Cast<ListViewItem>().ToList();
+        string fileFullPaths = 
+          string.Join(Environment.NewLine, selectedItems.Select(x => x.Name));
+        Clipboard.SetText(fileFullPaths);
+      }
+    }
+
+    private void listView1_SelectedIndexChanged(object sender, EventArgs e) {
+      this.toolStripStatusLabel1.Text = 
+        $"{this.listView1.SelectedItems.Count} selected ";
+    }
+
+    private void button2_Click(object sender, EventArgs e) {
+
+      // clear all items
+      this.treeView1.Nodes.Clear();
+
+      // initialize treeview
+      this.treeView1.Font = new Font("Consolas", 10f);
+
+      // create image list
+      ImageList imageList = new ImageList();
+      imageList.Images.Add("file", Image.FromFile("./images/icons8-file-100.png"));
+      imageList.Images.Add("folder", Image.FromFile("./images/icons8-folder-100.png"));
+      imageList.ImageSize = new Size(18, 18);
+      this.treeView1.ImageList = imageList;
+
+      // add items
+      TreeNode treeNode1 = this.treeView1.Nodes
+        .Add(key: "D:\\Dropbox", text: "Dropbox", imageKey: "folder", selectedImageKey: "folder");
+      treeNode1.Nodes.Add("D:\\Dropbox\\text1.txt", text: "text1.txt", imageKey: "file", selectedImageKey: "file");
+
+      TreeNode treeNode2 = this.treeView1.Nodes
+        .Add(key: "D:\\GoogleDrive", text: "GoogleDrive", imageKey: "folder", selectedImageKey: "folder");
+      treeNode2.Nodes.Add(key: "D:\\GoogleDrive\\Docs", text: "Docs", imageKey: "folder", selectedImageKey: "folder");
+      treeNode2.Nodes.Add(key: "D:\\GoogleDrive\\readme.md", text: "readme.md", imageKey: "file", selectedImageKey: "file");
+
+      TreeNode treeNode3 = this.treeView1.Nodes
+        .Add(key: "D:\\aaa.txt", text: "aaa.txt", imageKey: "file");
+    }
+  }
+}
